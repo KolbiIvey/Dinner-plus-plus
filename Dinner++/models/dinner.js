21c47b8f6a8ab9bd78ff649bf646277b74cc8069
KolbiIvey <kolbiivey@gmail.com>
2023-03-25 12:35:47 -0600

    filled out dinnerSchema

--- Dinner++/models/dinner.js
+++ Dinner++/models/dinner.js
@@ -3,19 +3,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const dinnerSchema = new Schema({
-    
-})
-
-
-
-
-
-
-
-
-
-
-
+    eventDate: {
+        type: Date,
+        required: true
+    },
+    eventLocation: {
+        type: String,
+        required: true
+    },
+    eventHost: {
+        type: String,
+        required: true
+    },
+    attendeeList: {
+        type: Array,
+        // add ref here for Oauth [ref]
+    },
+    foodList: {
+        type: Array,
+        //add embedded data here [embedded]
+    }}, {
+        timestamps: true
+});
 
 
 
