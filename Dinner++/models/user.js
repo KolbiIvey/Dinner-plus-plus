16f4cd143e4926adf4bc9690ceda5b1e34e982b7
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-25 14:59:17 -0600

    updated google oAuth in config and controllers, and updated header.ejs

---
+++ Dinner++/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Are there other values that come back from google?
+// Is this where we also need to include google calendar schema?
+
+const userSchema = new Schema({
+    name: String,
+    googleId: {
+      type: String,
+      required: true
+    },
+    email: String,
+    avatar: String
+  }, {
+    timestamps: true
+  });
+
+
+
+module.exports = mongoose.model('User', userSchema);
