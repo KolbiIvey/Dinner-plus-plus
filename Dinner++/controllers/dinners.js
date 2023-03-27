7edcdd3f6eff3cc52c0b4c41ff8ba3c3556c853f
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-27 01:39:46 -0600

    able to integrate and generate a response from openai

--- Dinner++/controllers/dinners.js
+++ Dinner++/controllers/dinners.js
@@ -1,7 +1,7 @@
 // created a dinner.js in controllers
 const Dinner = require('../models/dinner');
 
-
+const openai = require('../config/gpt');
 
 
 async function index(req, res) {   
@@ -27,25 +27,41 @@ async function create(req, res) {
         const dinner = new Dinner(req.body);
         await dinner.save();
         console.log(dinner);
+
+        // openai
+        const evtName = req.body.eventName
+        const evtHost = req.body.eventHost
+        const prompt = `Write an invitation for ${evtName} hosted by ${evtHost} in less than 200 characters.`
+        console.log(prompt)
+        const params = {
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0.5,
+            n: 1,
+        };
+
+        console.log(params)
+        const response = await openai.createCompletion(params);
+        
+        console.log(response.data.choices[0].text)
+        //return response.choices[0].text.trim();
+
         res.redirect('/dinners') //temp code, after show.ejs is coded out
         //redirect to '/dinners/${dinner._id}
     } catch (err) {
+        if (err.response){
+            console.log(err.response.status);
+            console.log(err.response.data);
+        }
         res.redirect('/dinners/new');
     }
 }
 
 
 
-
-
-
-
-
-
-
-
 module.exports = {
     index,
     new: newDinnerdate,
-    create
+    create,
+
 }
