b5e8d7719211d50625a3b137cdd88dcc96964bce
yutaokkots <yuta.okkotsu@gmail.com>
2023-03-27 22:13:35 -0600

    newFood and createFood functions semi-work. createFood function in controller/foods needs work

--- Dinner++/controllers/foods.js
+++ Dinner++/controllers/foods.js
@@ -3,26 +3,55 @@ const Dinner = require('../models/dinner');
 
 
 
-function newFood(req, res) {
+async function newFood(req, res) {
+    const dinner = await Dinner.findById(req.params.id)
     // may need to add async function for seeded data
-    res.render('foods/new', {title: 'Add A New Food', errorMsg: 'Not working'});
+    res.render(`foods/new`, {
+        title: 'Add A New Food', 
+        dinner: dinner});
 }
 
+async function createFood(req, res){
+    try {
+        const meal = await Food.create(req.body);
+        const dinner = await Dinner.findById(req.params.id)
+        // unable to still push the new meal to the dinner.foodList array
+        // need to incorporate an async ?
+        console.log(meal._id) // this is a 'new ObjectId('93847ja89234 hash')
+        const newMeal = await Food.findById(meal._id)
+        console.log(newMeal) // this is the object itself. 
+        dinner.foodList.push(newMeal._id)
+        
+        res.redirect(`${dinner._id}`)
+    } catch(err){
+        console.log(err);
+        res.status(500).send(err.message);
+    }
+}
 
+// drafted an asyn function here.
+// async function createFood(req, res){
+//     try {
+//         Promise.resolve().then(function() {
+//         const meal = Food.create(req.body);
+//         return meal
+//     }).then(async function(result){
+//         const dinner = await Dinner.findById(req.params.id)
+//         dinner.foodList.push(result._id)
+//         return dinner
+//     }).then(async function(result){
+//         res.redirect(`${result._id}`)
+//     })
+//     } catch(err){
+//         console.log(err);
+//         res.status(500).send(err.message);
+//     }
 
-
-
-
-
-
-
-
-
-
-
+// }
 
 
 
 module.exports = {
-    new: newFood
+    new: newFood,
+    createFood
 }
--- Dinner++/routes/foods.js
+++ Dinner++/routes/foods.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const foodsCtrl = require('../controllers/foods');
 
 //GET /dinners/foods/new
-router.get('/foods/new', foodsCtrl.new);
+router.get('/dinners/:id/foods/new', foodsCtrl.new);
 
-
-
+// POST / create
+router.post('/dinners/:id', foodsCtrl.createFood)
 
 
 
